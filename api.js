4807ae007f6716085a5a42a8f1f7af5cd39b7e3b
zachary <zacharysang@gmail.com>
2017-06-10 17:49:26 -0400

    Working on checking for existence

--- api.js
+++ api.js
@@ -1,6 +1,10 @@
+var env = require('./env');
 var express = require('express');
 var User = require('./models/user');
 var router = express.Router();
+var GoogleAuth = require('google-auth-library');
+var auth = new GoogleAuth;
+var client = new auth.OAuth2(env.GOOGLE_CLIENT_ID, '', '');
 
 router.get('/',function(req,res){
     resObj = {
@@ -24,6 +28,32 @@ router.use('/signup', function(req,res){
 router.get('/connect',function(req,res){
     
 });
+
+//checks if user exists in db already. if not, prompts for data
+router.use('/exists',function(req,res){
+    console.log(`req.body: ${req.body.token}`);
+    //extract the id_token from request
+    var token = req.body.token;
+    console.log(`token: ${token}`);
+
+    //verify id token is valid
+    /*client.verifyIdToken(
+    token,
+    env.GOOGLE_CLIENT_ID,
+    function(e, login) {
+      var payload = login.getPayload();
+      var userid = payload['sub'];
+      // If request specified a G Suite domain:
+      //var domain = payload['hd'];
+    });
+    */
+
+    //lookup token in db
+    res.send('received');
+
+});
+
+
 
 
 module.exports = router;
