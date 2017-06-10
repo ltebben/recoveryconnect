8bffdca40ae18fdf3b065937d98e51e139c98a80
Laura Tebben <tebbenla@mail.uc.edu>
2017-06-10 16:01:11 -0400

    add user_id to schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
+    user_id: String,
     firstName: String,
     neighborhood: String,
     gender: String,
     age: Number,
-    status: String
+    status: String 
 });
 
 module.exports = mongoose.model("User", userSchema);
