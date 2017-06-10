71ed43f2dd8cd126fedb348bb1b95fb2621620a4
Laura Tebben <tebbenla@mail.uc.edu>
2017-06-10 16:45:33 -0400

    Add signup code

--- api.js
+++ api.js
@@ -12,6 +12,14 @@ router.get('/',function(req,res){
     res.send(resObj);
 });
 
+router.use('/signup', function(req,res){
+    User.firstname = req.user.firstName;
+    User.neighborhood = req.user.neighborhood;
+    User.gender = req.user.gender;
+    User.age = req.user.age;
+    User.status = req.user.status;
+});
+
 router.get('/connect',function(req,res){
     
 });
