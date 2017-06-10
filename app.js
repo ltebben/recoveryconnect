730dfdda2b874da17c2a9ca886cbb94a305aec4b
zachary <zacharysang@gmail.com>
2017-06-10 14:57:14 -0400

    Added sass support. Set up view basis

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,11 +20,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+
+//load style preprocessing middleware
+app.use(require('node-sass-middleware')({
+  root: path.join('public','stylesheets'),
+  src: 'scss',
+  dest: '.',
+  debug: true,
+  outputStyle: 'compressed',
+  force: true,
+  error:function(err){
+    console.log("Sass compilation error: " + err);
+  }
 }));
 
 
