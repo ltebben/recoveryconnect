554a3a0b5e432e5639dce02196d1491b61586285
zachary <zacharysang@gmail.com>
2017-06-11 01:46:25 -0400

    Fixed up form a bit

--- public/js/client.js
+++ public/js/client.js
@@ -2,9 +2,11 @@ function onSignIn(user){
     var profile = user.getBasicProfile();
     var id_token = profile.getEmail()//user.getAuthResponse().id_token;
    
-   //put the google id into the form
-    var idInput = document.querySelector('input[name="user_id"]');
-    idInput.setAttribute("value",id_token);
+   //put the google email into the form
+    document.querySelector('input[name="user_id"]').setAttribute("value",profile.getEmail());
+
+    //put first name into the form
+    document.querySelector('input[name="firstName"]').setAttribute("value",profile.getName().substring(0,profile.getName().indexOf(' ')));
 
     //check if the user exists, if not, need to prompt for info
     //if(fetch){
